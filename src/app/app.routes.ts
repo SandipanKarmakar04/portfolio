d36df27819182ed99022ef9269122f2e9442a65b
Sandipan <sandipan.sk7@gmail.com>
2025-02-09 02:31:07 +0530

    upload all the components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home',component:HomeComponent},
+    {path:'experience',component:ExperienceComponent},
+    {path:'education',component:EducationComponent},
+    {path:'projects',component:ProjectsComponent},
+    {path:'about',component:AboutComponent}
+];
