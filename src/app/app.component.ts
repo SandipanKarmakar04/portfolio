874cf0a007780904d4fcab25b01a54efcd2e0de8
Sandipan <sandipan.sk7@gmail.com>
2025-02-19 01:23:54 +0530

    make changes in about component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RouterLink,NavbarComponent,NavbarComponent,FooterComponent],
+  imports: [RouterOutlet,RouterLink,NavbarComponent,NavbarComponent,FooterComponent,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
