93d7770e68412296f8742e1008fe35351842bbe3
Sandipan <sandipan.sk7@gmail.com>
2025-04-01 04:02:43 +0530

    modified experience.component.html and css files

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-education',
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './education.component.html',
   styleUrl: './education.component.css'
 })
